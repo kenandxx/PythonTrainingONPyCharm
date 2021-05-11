e5f2580613b3de506de0f8718d6636be11959eb4
Jacky <kenandxx@gmail.com>
2021-05-11 11:25:32 +0900

    Add 5.2

---
+++ 5 字典/5.2 漂亮打印.py
@@ -0,0 +1,16 @@
+import pprint #short for Pretty-print
+
+message = 'I like cats and girls. They better be pretty, but that does not REALLY matter'
+count = {}
+count1 = {1: 'First', 2: 'Second'}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+print(count)
+pprint.pprint(count) #帮你做了改行，排序
+print(count1)
+pprint.pprint(count1)
+#print(pprint.pformat(count1)) #
+
